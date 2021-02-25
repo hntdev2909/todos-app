5703bccc79dc1486d9b20622898d42ceb772f698
Ngoc Thinh <ngocthinh.dev@gmail.com>
2021-02-26 01:44:51 +0700

    Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# TODOS APP
+
+- A project built by me with CSS Template of [Klerith](https://github.com/Klerith/TODO-CSS-Template)
+
+- This project, I'm using HTML - CSS by Template CSS TODOS App of Klerith and Javascript Vanilla
+
+- Main function of the application: Add, Update, Delete
+
+[Link to review](https://todos-mvc-app.netlify.app/)
+
+With the guidance of **[Son Dang F8](https://fullstack.edu.vn/courses)**
---
+++ component/TodoItem.js
@@ -0,0 +1,34 @@
+import html from '../core.js';
+
+function TodoItem({ todo, index, editIndex }) {
+	return html`
+		<li
+			class="${todo.isComplete && 'completed'} ${editIndex === index &&
+			'editing'}"
+		>
+			<div class="view">
+				<input
+					class="toggle"
+					type="checkbox"
+					${todo.isComplete && 'checked'}
+					onchange="dispatch('toggle', ${index})"
+				/>
+				<label ondblclick="dispatch('startEdit', ${index})"
+					>${todo.title}</label
+				>
+				<button
+					class="destroy"
+					onclick="dispatch('destroy', ${index})"
+				></button>
+			</div>
+			<input
+				class="edit"
+				value="${todo.title}"
+				onkeyup="event.keyCode === 13 && dispatch('endEdit', this.value.trim()) || event.keyCode === 27 && dispatch('cancelEdit')"
+				onblur="dispatch('endEdit', this.value.trim())"
+			/>
+		</li>
+	`;
+}
+
+export default TodoItem;
